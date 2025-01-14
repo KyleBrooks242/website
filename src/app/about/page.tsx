c10eac35c99675d8260aacae736721dd7a7a3d7a
Kyle Brooks <kyle.brooks242@gmail.com>
2025-01-14 00:10:10 -0500

    Clean up some typos, add Churdle to Project Page, rewrite some content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,12 +9,18 @@ export default async function About() {
             One that provides a ton of fulfillment, but at the same time, one that sticks me in front of a computer for an unhealthy amount of time. 
           </p>
           <p>
-            Growing up, my family didn't travel much. We had an annual beach trip to Daytona Beach that my brother and I eagerly looked forward to every year. 
+            While I appreciate A/C and indoor plumbing a great deal, I do enjoy getting outside from time to time. Hiking, backpacking, climbing, and snowboarding are all activities
+            I try to make time for throughout the year. I find I don't need much, but I do need it often. It's all too easy to stay cooped up inside on
+            the extra hot or extra cold days, but I quickly find myself falling into a slump when that happens. 
+          </p>
+          <p>
+            Growing up, my family didn't travel much. We had an annual beach trip to Daytona, Florida that my brother and I eagerly looked forward to every year. 
             Outside of that, there wasn't much. In college, many of my friends studied abroad- traveling to England, Ireland, Germany, and Spain. As an absolutely broke 
             college student who was paying his own way through college, this was out of my price range. In fact, I became an RA my sophomore through senior year in order 
-            to afford housing. Besides my natural interest in Computer Science, one of the huge benefits I foresaw was the opportunities it would provide. One of my professors
-            insisted that, after graduating, I shouldn't accept any position less than 75k/year. Well, that may have been true for the top of the class students, or those
-            applying for positions in bigger cities. 
+            to afford housing. Besides my natural interest in Computer Science, one of the huge benefits I foresaw was the opportunities it would provide to afford traveling.
+            I took my first trip out of the country when I was 25. My brother and I did a 5 day hike through Peru, ending in Machu Pichu. Since then, I've ticked several more
+            countries off the list, but there are still countless places I hope to one day visit. 
+          
           </p>
         </main>
       </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,14 @@
+
 export default async function Projects() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20  sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-         <h1 className="header">Projects Me</h1>
+         <a className="project link" target="_blank" href="https://churdle.vercel.app/" rel="noopener noreferrer">Churdle</a>
          <p>
-            Add your projects here!
-        </p>
+          Churdle is a copy of the popular Wordle word game. I decided to see how closely I could replicate it without looking at the source code or following 
+          any sort of tutorial.  So it wasn't an <strong>exact</strong> copy, I instead made a 6 letter variation and started to implement the idea of a 'bomb letter'. As far as I know,
+          that was never fully finshed... so the results might be more explosive than intended.
+         </p>
         </main>
       </div>
     );
