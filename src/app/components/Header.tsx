a5c75a184f808c6b0479e29d16aa9b474c611599
Kyle Brooks <kyle.brooks242@gmail.com>
2025-01-14 21:56:40 -0500

    More ui tweaking

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,12 +5,14 @@ export default function Header () {
 
     return (
         <div className="navbar bg-primary fixed">
-        <a className="btn btn-ghost mx-0 px-2 text-base sm:text-sm md:text-xl lg:text-lg xl:text-xl">Kyle Brooks</a>
-        <Link className="link link-hover nav-link" href='/'>Home</Link>
-        <Link className="link link-hover nav-link" href='/blog'>Blog</Link>
-        <Link className="link link-hover nav-link" href='/projects'>Projects</Link>
-        <Link className="link link-hover nav-link" href='/about'>About</Link>
-        <Link className="link link-hover nav-link" href='/contact'>Contact</Link>
+        <a className="btn btn-ghost mx-0  text-base sm:text-sm md:text-xl lg:text-lg xl:text-xl">Kyle Brooks</a>
+        {/* <div className='nav-links'> */}
+          <Link className="link link-hover nav-link" href='/'>Home</Link>
+          <Link className="link link-hover nav-link" href='/blog'>Blog</Link>
+          <Link className="link link-hover nav-link" href='/projects'>Projects</Link>
+          <Link className="link link-hover nav-link" href='/about'>About</Link>
+          <Link className="link link-hover nav-link" href='/contact'>Contact</Link>
+        {/* </div> */}
       </div>
     )
 }
