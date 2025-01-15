439f83f86dec002345ffa5bffcf89aaf26f897ef
Kyle Brooks <kyle.brooks242@gmail.com>
2025-01-15 14:30:24 -0500

    Update resume to include website link. Add under construction banner and fix spacing in blog paragraphs

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header () {
           <a className="btn btn-ghost mx-0  text-base sm:text-sm md:text-xl lg:text-lg xl:text-xl">Kyle Brooks</a>
 
           <Link className="link link-hover nav-link pr-4" href='/'>
-            <HomeIcon className="sm:w-4 md:w-6 w-4 h-4 mr-1" href='/'/>
+            <HomeIcon className="sm:w-4 md:w-6 w-4 h-4 mr-1"/>
             <span className='hidden sm:inline'>Home</span>
           </Link>
 
