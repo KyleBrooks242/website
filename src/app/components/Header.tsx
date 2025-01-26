26c87c592e1cd298703e69b0b95173c1cf693235
Kyle Brooks <kyle.brooks242@gmail.com>
2025-01-26 10:06:59 -0500

    Remove console log

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,7 +13,6 @@ export default function Header () {
 )
 
   const handleThemeToggle = () => {
-    console.log('handling theme toggle!');
     setTheme(theme === lightTheme ? darkTheme : lightTheme);
   };
 
