5e9aa42334c1461709cc8a8400095efabc2344ea
Kyle Brooks <kyle.brooks242@gmail.com>
2025-01-27 14:16:04 -0500

    Home page tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,17 +14,17 @@ export default function Home() {
 
         <h1 className='h1'>Welcome!</h1>
         <p>
-          If you have stumbled upon this page, chances are you are a potential employer, looking to hire a certain Kyle Brooks. You noticed he was bold enough
+          If you have stumbled upon this page, chances are you are a potential employer. You noticed he was bold enough
           to include his website in his LinkedIn, so you figured you'd go and take a look. Do not let the appearances of this page deceive you. What he 
           lacks in design taste, he more than makes up for with his charm and his willingness to learn. So please-- take a look around, but be merciful in your 
           judgments.
         </p>
         <p>
           The purpose of this site is a place for me to experiment and learn. Where possible, I'll be avoiding the blind copy/paste of
-          code snippets/configs/etc. that I've been so fond of in the past. Nothing is more frustrating than a jumbled mess of code and config files
+          code snippets/configs/etc. Nothing is more frustrating than a jumbled mess of code and config files
           that aren't building as expected/ rendering as intended and not knowing where to even begin looking to solve the problem.
         </p>
-        <p>Check out my blog to follow the construction progress of this page!</p>
+        <p>Check out my blog to follow the construction progress of the site</p>
       </main>
     </div>
   );
